977ec78b0c1e72c5aaff30e675f69844127a36e8
Bis-the-nub <jui20010815@gmail.com>
2025-09-17 22:18:10 -0400

    second draft while checking and amending some contents

--- hw1 r code.R
+++ hw1 r code.R
@@ -1,5 +1,7 @@
 ##Importing data
 library(moderndive)
+library(ggplot2)
+library(tidyverse)
 data("early_january_weather")
 
 ##Problem 1
@@ -13,15 +15,16 @@ ncol(early_january_weather)
 mean(early_january_weather$temp)
 
 #plotting scatter plot 
-library(ggplot2)
 Sys.setlocale("LC_TIME", "C") # transfer date from mandarin to english
-ggplot(early_january_weather, aes(x = time_hour, y = temp, color = humid)) +
-  geom_point(alpha = 0.5) 
+ggplot(early_january_weather, 
+       aes(x = time_hour, 
+           y = temp, 
+           color = humid)) +
+       geom_point(alpha = 0.5) 
 # Save plot
 ggsave("early_january_weather.png", width = 8, height = 5, dpi = 300)
 
 ##Problem 2
-library(tidyverse)
 set.seed(123)  # reproducibility
 
 # build tibble
@@ -29,8 +32,8 @@ tb <- tibble(
   sample = rnorm(10),
   positive = sample > 0,
   chr_10 = c("This","class","is","important",
-            "for","my","future","studying","in","biostatistics"),
-  factor_level = factor(rep(c("A", "B", "C"), length.out = 10))
+             "for","my","future","studying","in","biostatistics"),
+              factor_level = factor(rep(c("A", "B", "C"), length.out = 10))
 )
 
 # attempting to calculate average
